4d1a5a457265144ba35acfcedeefb730b26348d4
ManchildProductions <7bejt5ooqf8p@opayq.net>
2021-09-14 00:26:08 -0400

    Work on replacing branding

---
+++ malepoon/branding/official/pref/malepoon-branding.js
@@ -0,0 +1,27 @@
+#filter substitution
+#filter emptyLines
+#include ../../shared/pref/preferences.inc
+#include ../../shared/pref/uaoverrides.inc
+
+pref("startup.homepage_override_url","about:logo");
+pref("app.releaseNotesURL", "https://github.com/ManchildProductions/Male-Poon/releases");
+
+// Enable Firefox compatmode by default.
+pref("general.useragent.compatMode", 2);
+pref("general.useragent.compatMode.gecko", true);
+pref("general.useragent.compatMode.firefox", true);
+
+// Enable dynamic UA updates
+pref("general.useragent.updates.enabled", true);
+pref("general.useragent.updates.interval", 86400); // Once per day
+pref("general.useragent.updates.retry", 7200);     // Retry getting update every 2 hours if failed
+pref("general.useragent.updates.url", "https://dua.palemoon.org/?app=palemoon&version=%APP_VERSION%&channel=%CHANNEL%");
+
+// Geolocation
+pref("geo.wifi.uri", "https://pro.ip-api.com/json/?fields=lat,lon,status,message&key=K3TirHYiysBjnmD");
+
+// ========================= updates ========================
+// Updates disabled (Mac, etc.)
+pref("app.update.enabled", false);
+pref("app.update.url", "");
+#endif
---
+++ malepoon/build.mk
@@ -0,0 +1,63 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+installer:
+	@$(MAKE) -C malepoon/installer installer
+
+package:
+	@$(MAKE) -C malepoon/installer make-archive
+
+l10n-package:
+	@$(MAKE) -C malepoon/installer make-langpack
+
+mozpackage:
+	@$(MAKE) -C malepoon/installer
+
+package-compare:
+	@$(MAKE) -C malepoon/installer package-compare
+
+stage-package:
+	@$(MAKE) -C malepoon/installer stage-package make-buildinfo-file
+
+install::
+	@$(MAKE) -C malepoon/installer install
+
+clean::
+	@$(MAKE) -C malepoon/installer clean
+
+distclean::
+	@$(MAKE) -C malepoon/installer distclean
+
+source-package::
+	@$(MAKE) -C malepoon/installer source-package
+
+upload::
+	@$(MAKE) -C malepoon/installer upload
+
+source-upload::
+	@$(MAKE) -C malepoon/installer source-upload
+
+hg-bundle::
+	@$(MAKE) -C malepoon/installer hg-bundle
+
+l10n-check::
+	@$(MAKE) -C malepoon/locales l10n-check
+
+ifdef ENABLE_TESTS
+# Implemented in testing/testsuite-targets.mk
+
+mochitest-browser-chrome:
+	$(RUN_MOCHITEST) --browser-chrome
+	$(CHECK_TEST_ERROR)
+
+mochitest:: mochitest-browser-chrome
+
+.PHONY: mochitest-browser-chrome
+
+mochitest-metro-chrome:
+	$(RUN_MOCHITEST) --metro-immersive --browser-chrome
+	$(CHECK_TEST_ERROR)
+
+
+endif
